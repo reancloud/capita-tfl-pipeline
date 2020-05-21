7a629599b5d5198595cf5af316b086b32b17319e
Ritika Joshi <ritika.joshi@hitachivantara.com>
2020-05-21 16:24:29 -0400

    modifying name

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@
 
 # A machine-readable name for your project.
 #  - HCAP DevSecOps is able to derive other default settings from your project's name.
-set :application, "capita"
+set :application, "capitatfl"
 
 # The project's application type
 #  - HCAP DevSecOps will be changing type :bootstrap to another name in a future release.
