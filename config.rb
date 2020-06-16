aea7de828d5754714df0833675f79d1c6efe89a0
Ritika Joshi <ritika.joshi@hitachivantara.com>
2020-06-16 11:46:40 -0400

    Adding functional test

--- config.rb
+++ config.rb
@@ -135,8 +135,8 @@ set :infra_test_tool, :reantest
 #  - HCAP DevSecOps supports a simple syntax for declaring websites to be tested by HCAP Test.
 #  - HCAP DevSecOps will default to running all declared functional tests and load tests against
 #    all declared websites, unless you specifically declare which tests to run against which websites.
-#website :mysite, protocol: :http, host: "dev-joe-demo.eastus.cloudapp.azure.com", path: "/SpringBootCRUDApp", port: 9191
-website :mysite, protocol: :http
+website :mysite, protocol: :http, host: "dev-joe-demo.eastus.cloudapp.azure.com", path: "/SpringBootCRUDApp", port: 9191
+
 
 # A list of HCAP Test functional tests to be run, with a minimal number of options required.
 #  - HCAP DevSecOps automatically applies default test execution options to each test in the list below,
